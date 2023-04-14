a882f130b1c9f66fc9636e3136fcc201fce21b3e
AdrianSiwek <105585774+AdrianSiwek@users.noreply.github.com>
2023-04-14 21:28:51 +0200

    update hooks

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,38 +1,33 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import styles from './Modal.module.css';
 import PropTypes from "prop-types";
 
 
-class Modal extends Component {
+const Modal = ({onClose, image}) => {
 
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleModalKeyDown);
-  }
-  
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleModalKeyDown);
-  }
-
-  handleModalKeyDown = (event) => {
+  useEffect(() => {
+    const handleModalKeyDown = (event) => {
         if (event.code === "Escape") {
-            return this.props.onClose();
+            return onClose();
         }
   };
-  
-  
+    window.addEventListener("keydown", handleModalKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleModalKeyDown);
+    };
+  }, [onClose]);
 
   
-  render() { 
-    const {image} = this.props
+  
     return (
-      <div className={styles.overlay} onClick={this.props.onClose}>
+      <div className={styles.overlay} onClick={onClose}>
         <div className={styles.modal}>
           <img src={image} alt="" />
         </div>
       </div>
     );
   }
-}
+
 
 Modal.propTypes = {
     onClose: PropTypes.func,
